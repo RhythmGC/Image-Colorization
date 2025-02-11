7be267c77bdaf0135a1bed4c5f0c9e26cb04a09e
RhythmGC <125289048+RhythmGC@users.noreply.github.com>
2025-02-12 00:39:18 +0700

    update gitignore and have grayscaling code

--- DataProcessing/grayscaling.py
+++ DataProcessing/grayscaling.py
@@ -1,29 +1,39 @@
 import cv2
 import shutil
+import os
 
 def grayscale(image, fileName,temp_output_path):
     # Convert to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Save the grayscale image
     cv2.imwrite(f"{temp_output_path}/grayscale-{fileName}", gray_image)
-    print(f"Image saved in the folder {temp_output_path}/{fileName}")
-    print(fileName)
-    print(image.shape)
-    print(gray_image.shape)
+    # print(f"Image saved in the folder {temp_output_path}/{fileName}")
+    # print(fileName)
+    # print(image.shape)
+    # print(gray_image.shape)
     return gray_image
 
 def copy_paste(source, destination):
     shutil.copy(source, destination)  # Copies content, permissions, and metadata
-    print("File copied with metadata successful!")
 
 def processImage(input_path,temp_output_path):
     # Read the image
     image = cv2.imread(input_path)
     filename = input_path.split("/")[-1]
     # Convert the image to grayscale
-    copy_paste(input_path, f"{temp_output_path}/{filename}")
+    # copy_paste(input_path, f"{temp_output_path}/{filename}")
     gray_image = grayscale(image,filename,temp_output_path)
     return gray_image
 
+def processFolder(input_folder, output_folder):
+    image_files = [f for f in os.listdir(input_folder) if f.endswith(('jpg', 'jpeg', 'png'))]
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    for image_file in image_files:
+        print(f"Processing {image_file}")
+        processImage(f"{input_folder}/{image_file}", output_folder)
 
-processImage("Data/Frame/VIDEO4/frame_0286.jpg","Data/Presentation1/Data4")
+processFolder('Data/original/Color_image/VIDEO1','Data/original/grayscale-image/VIDEO1')
+processFolder('Data/original/Color_image/VIDEO2','Data/original/grayscale-image/VIDEO2')
+processFolder('Data/original/Color_image/VIDEO3','Data/original/grayscale-image/VIDEO3')
+processFolder('Data/original/Color_image/VIDEO4','Data/original/grayscale-image/VIDEO4')
